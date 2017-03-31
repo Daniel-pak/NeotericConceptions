ac52a9509416771fee3d8fd0f6f08dff1b6f4355
Daniel Pak <PakJuhnMan@gmail.com>
2017-03-31 13:06:10 -0600

    Edited blogs section of the project

--- TheCulture/back-end/routes/blogRoutes.js
+++ TheCulture/back-end/routes/blogRoutes.js
@@ -7,6 +7,14 @@ blogRoutes.get('/', function(req, res) {
         if (err) return res.status(500).send(err);
         res.send(blogs);
     })
-});
+})
+
+blogRoutes.get('/:id', function(req, res){
+    Blog.findById(function(err, blog){
+        if (err) return res.status(500).send(err);
+        if (!blog) return res.status(404).send({success: false, message: "Blog post not found"});
+        return res.status(202).send(blog)
+    })
+})
 
 module.exports = blogRoutes; 
