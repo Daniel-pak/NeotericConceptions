b854bdc9a08d539752767829a75049faf7d376b4
dbogs425 <dbolognino425@gmail.com>
2017-04-03 09:30:13 -0400

    .otherwise

--- TheCulture/public/app.js
+++ TheCulture/public/app.js
@@ -1,25 +1,28 @@
 angular.module("TheCultureApp", ['ngRoute'])
 
-    .config(["$routeProvider", function ($routeProvider) {
-        $routeProvider
-            .when('/dashboard', {
+.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+        .when('/dashboard', {
+        controller: "HomeController",
+        templateUrl: "views/dashboard/dashboard.html"
+    })
+        .when('/home', {
             controller: "HomeController",
-            templateUrl: "views/dashboard/dashboard.html"
+            templateUrl: "home/home.html"
         })
-            .when('/home', {
-                controller: "HomeController",
-                templateUrl: "home/home.html"
-            })
-            .when('/blog', {
-                controller: "BlogController",
-                templateUrl: "blog/blog.html"
-            })
-            .when('/podcast', {
-                controller: "PodcastController",
-                templateUrl: "podcasts/podcasts.html"
-            })
-//            .when('/blog/:id', {
-//                controller: "BlogPostDetailController", 
-//                templateUrl: "blogDetail/blogDetail.html"
-//            })
+        .when('/blog', {
+            controller: "BlogController",
+            templateUrl: "blog/blog.html"
+        })
+        .when('/podcast', {
+            controller: "PodcastController",
+            templateUrl: "podcasts/podcasts.html"
+        })
+        .otherwise('/dashboard', {
+            redirectTo: "/dashboard"
+        });
+        //            .when('/blog/:id', {
+        //                controller: "BlogPostDetailController", 
+        //                templateUrl: "blogDetail/blogDetail.html"
+        //            })
     }]);
