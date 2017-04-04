d95a49c8c283ab7c9bdd1f8305faf790d524b415
Daniel Pak <PakJuhnMan@gmail.com>
2017-04-04 14:14:01 -0600

    Addedadded different websites to repo, freelance work

---
+++ gabby_website/backend/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express(); 
+var path = require('path');
+var port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.listen(port, function() { 
+    console.log('connected to port ' + port);
+})
