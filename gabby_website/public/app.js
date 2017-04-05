f104c3424ebc7e48b2f140774a9db40ea44257d4
Daniel Pak <PakJuhnMan@gmail.com>
2017-04-05 17:56:43 -0600

    edited gabby portfolio

--- gabby_website/public/app.js
+++ gabby_website/public/app.js
@@ -1,8 +1,21 @@
 $(document).ready(function () {
 
     $('body').scrollspy({
-        target: '#scrollSpyList'
-    }) 
-    
+        target: '.scrollSpyList'
+    });
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+    })
+    $('#vice').hide();
+
     //select options here
+    $('#source').on("change", function () {
+        if ($('#source').val() == 'sourceNY') {
+            $('#nyPost').show();
+            $('#vice').hide();
+        } else if ($('#source').val() == 'sourceVice') {
+            $('#nyPost').hide();
+            $('#vice').show();
+        }
+    })
 })
